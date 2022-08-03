4a0c14ecfcd126087345b578cb83c9d475719aac
Santi Arroyo <santi.arroyo551@gmail.com>
2022-08-03 11:01:26 -0400

    made first function (makeRequest)

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ let currentPage = 0;
 let pages;
 let query;
 let cooldown = false;
+
+function makeRequest() {
+    if (cooldown) {
+      return Promise.reject("Too soon");
+    }
+  
+    cooldown = true;
+  
+    const url = `https://musicbrainz.org/ws/2/artist?query=${query}&fmt=json&limit=${pageLength}&offset=${
+      currentPage * pageLength
+    }`;                       //fetching data from API
+    return fetch(url).then((res) => {
+      setTimeout(() => {
+        cooldown = false;
+      }, 1000);              //setTimeout will prevent user from clicking more than once a second
+      return res.json();
+    });
+  }
